9551ea1c4c228ceba942fc7cda02e6f152a84bf5
Maya Moshe <maya.moshe@post.runi.ac.il>
2025-12-27 16:59:03 +0200

    1

--- Koch.java
+++ Koch.java
@@ -29,6 +29,26 @@ public class Koch {
 	public static void curve(int n, double x1, double y1, double x2, double y2) {
 		StdDraw.line(x1, y1, x2, y2);
 		//// Write the rest of your code below.
+		if (n == 0) {
+        StdDraw.line(x1, y1, x2, y2);
+        return;
+		}
+		double xA = x1 + (x2 - x1) / 3.0;
+    	double yA = y1 + (y2 - y1) / 3.0;
+    	double xB = x1 + 2.0 * (x2 - x1) / 3.0;
+    	double yB = y1 + 2.0 * (y2 - y1) / 3.0;
+		double vx = xB - xA;
+    	double vy = yB - yA;
+    	double cos60 = 0.5;
+    	double sin60 = Math.sqrt(3) / 2.0;
+    	double xC = xA + vx * cos60 - vy * sin60;
+    	double yC = yA + vx * sin60 + vy * cos60;
+
+    // Recurse on the 4 resulting segments
+    curve(n - 1, x1, y1, xA, yA);
+    curve(n - 1, xA, yA, xC, yC);
+    curve(n - 1, xC, yC, xB, yB);
+    curve(n - 1, xB, yB, x2, y2);
 	}
 
     /** Gets n, and draws a Koch snowflake of n edges in the standard canvass. */
@@ -38,5 +58,16 @@ public class Koch {
 		StdDraw.setXscale(0, 1.1);
 		// Draws a Koch snowflake of depth n
 		//// Write the rest of your code below.
+		StdDraw.setYscale(0, 1.1);
+    	StdDraw.setXscale(0, 1.1);
+		double x1 = 0.1, y1 = 0.15;
+    	double x2 = 1.0, y2 = 0.15;
+    	double x3 = 0.55, y3 = 0.15 + (x2 - x1) * Math.sqrt(3) / 2.0;
+		curve(n, x1, y1, x2, y2);
+    	curve(n, x2, y2, x3, y3);
+    	curve(n, x3, y3, x1, y1);  
+    	curve(n, x2, y2, x3, y3);
+    	curve(n, x3, y3, x1, y1);
+
 	}
 }
